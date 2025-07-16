180be4a3f04e9c9adeb9d49fe5f60e744727a3fc
ddaaniel <daniel0333v@gmail.com>
2025-07-16 16:17:27 -0300

    ...

--- hypr/scripts/set-wallpaper.sh
+++ hypr/scripts/set-wallpaper.sh
@@ -3,7 +3,7 @@
 sleep 1
 
 # Define o wallpaper com transição
-swww img "$HOME/Imagens/backgrounds/yoal-desurmont-jqgsM3B9Fpo-unsplash.jpg" \
+swww img "$HOME/Imagens/backgrounds/mohamed-musthafa-musthaq-ahamed-_Yu1qtQ9bVI-unsplash.jpg" \
   --transition-type grow \
   --transition-duration 1 \
   --transition-fps 60 \
